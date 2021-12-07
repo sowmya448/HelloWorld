49efe20ad1ba4cee032d64ae769e38665a76a0e0
sowmya448 <sowmya.mallika@gmail.com>
2021-12-07 17:48:13 -0500

    first commit

---
+++ src/com/test/HelloSowmya.java
@@ -0,0 +1,12 @@
+//this class will print helloworld
+package com.test;
+
+public class HelloSowmya {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,sowmya!");
+
+	}//end of main method
+
+}/*end of 
+class*/
